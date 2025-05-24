2ed157e2bbb285d9d857c8fcdc2f9338a39eb883
BeckyZhuWu <zhuwub@gmail.com>
2025-05-23 18:24:43 -0600

    feat:icons and routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
-import { AuthLayout } from './layout/AuthLayout'
-import { Login } from './pages/Login'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthLayout } from "./layout/AuthLayout";
+import { Login } from "./pages/Login";
+import { MainRoutes } from "./routes/MainRoutes";
 
 function App() {
-
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<AuthLayout/>}>
-          <Route index element={<Login/>}/>     
+        <Route path="/login" element={<AuthLayout />}>
+          <Route index element={<Login />} />
         </Route>
+        <Route path="/*" element={<MainRoutes />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
