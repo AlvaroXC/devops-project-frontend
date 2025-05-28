d38c1266a07db237e2640abab03a6e000054ff92
BeckyZhuWu <zhuwub@gmail.com>
2025-05-27 18:25:23 -0600

    fix:merge problems fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthLayout } from "./layout/AuthLayout";
 import { Login } from "./pages/Login";
-<<<<<<< Updated upstream
-import { Drivers } from "./pages/Drivers";
-import { Register } from "./pages/Register";
-import { AuthProvider } from './context/AuthProvider'
-import { VehiclesProvider } from './context/VehicleProvider'
-import { Dashboard } from "./pages/Dashboard";
-import RutaProtegida  from "./layout/RutaProtegida";
-import { RegisterVehicle } from "./pages/RegisterVehicle";
-=======
 import { Register } from "./pages/Register";
 import { AuthProvider } from "./context/AuthProvider";
 import { VehiclesProvider } from "./context/VehicleProvider";
@@ -18,28 +9,12 @@ import RutaProtegida from "./layout/RutaProtegida";
 import { RegisterVehicle } from "./pages/RegisterVehicle";
 import { RegisterDrivers } from "./pages/RegisterDrivers";
 import { DriversProvider } from "./context/DriverProvider";
->>>>>>> Stashed changes
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <VehiclesProvider>
-<<<<<<< Updated upstream
-          <Routes>
-            <Route path="/" element={<AuthLayout />}>
-              <Route index element={<Login />} />
-              <Route path="/register" element={<Register />} />
-            </Route>
-
-            <Route path="/admin" element={<RutaProtegida />}>
-              <Route index element={<Dashboard />} />
-              <Route path="drivers" element={<Drivers/>} />
-              <Route path="vehicles" element={<RegisterVehicle/>} />
-            </Route>
-            {/* <Route path="/*" element={<MainRoutes />} /> */}
-          </Routes>
-=======
           <DriversProvider>
             <Routes>
               <Route path="/" element={<AuthLayout />}>
@@ -55,7 +30,6 @@ function App() {
               {/* <Route path="/*" element={<MainRoutes />} /> */}
             </Routes>
           </DriversProvider>
->>>>>>> Stashed changes
         </VehiclesProvider>
       </AuthProvider>
     </BrowserRouter>
